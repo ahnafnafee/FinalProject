847f7ec63c7e0a2bbf66cfcfdf387e57022d8466
Ahnaf An Nafee <nafee01@gmail.com>
2020-09-05 03:08:05 -0400

    Parkour concept added

---
+++ Assets/Imported/SuperHot/Scripts/EnemyScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class EnemyScript : MonoBehaviour
+{
+    Animator anim;
+    public bool dead;
+    public Transform weaponHolder;
+
+    public Camera myCam;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(RandomAnimation());
+
+        if (weaponHolder.GetComponentInChildren<WeaponScript>() != null)
+            weaponHolder.GetComponentInChildren<WeaponScript>().active = false;
+
+    }
+
+    void Update()
+    {
+        if (!dead)
+        {
+            var position = myCam.transform.position;
+            transform.LookAt(new Vector3(position.x, 0, position.z));
+        }
+    }
+
+    public void Ragdoll()
+    {
+        anim.enabled = false;
+        BodyPartScript[] parts = GetComponentsInChildren<BodyPartScript>();
+        foreach (BodyPartScript bp in parts)
+        {
+            bp.rb.isKinematic = false;
+            bp.rb.interpolation = RigidbodyInterpolation.Interpolate;
+        }
+        dead = true;
+
+        if (weaponHolder.GetComponentInChildren<WeaponScript>() != null)
+        {
+            WeaponScript w = weaponHolder.GetComponentInChildren<WeaponScript>();
+            w.Release();
+
+        }
+    }
+
+    public void Shoot()
+    {
+        if (dead)
+            return;
+
+        if (weaponHolder.GetComponentInChildren<WeaponScript>() != null)
+            weaponHolder.GetComponentInChildren<WeaponScript>().Shoot(GetComponentInChildren<ParticleSystem>().transform.position, transform.rotation, true);
+    }
+
+    IEnumerator RandomAnimation()
+    {
+        anim.enabled = false;
+        yield return new WaitForSecondsRealtime(Random.Range(.1f, .5f));
+        anim.enabled = true;
+
+    }
+}
---
+++ Assets/Scripts/WeaponSwitcher1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher1 : MonoBehaviour
+{
+    public GameObject[] weapons;
+    public GameObject grappleGun;
+
+    private void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.Alpha1)) weapons[0].SetActive(true);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) grappleGun.GetComponent<GrapplingGun>().isActive = true;
+    }
+
+    // private void DeactivateAll()
+    // {
+    //     for (int i = 0; i < weapons.Length; i++)
+    //     {
+    //         //exclude grappleGun
+    //         if (i != 1)
+    //             weapons[i].SetActive(false);
+    //         else
+    //             grappleGun.GetComponent<GrapplingGun>().isActive = false;
+    //     }
+    // }
+}
