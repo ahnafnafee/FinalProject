100c157b897247cd520e3f6e57b62194042ad51b
Ahnaf An Nafee <nafee01@gmail.com>
2020-09-06 11:19:09 -0400

    Volume changer added

---
+++ Assets/Scripts/AudioSettings.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSettings : MonoBehaviour {
+
+     FMOD.Studio.Bus Master;
+     float MasterVolume = 1f;
+
+     void Awake ()
+     {
+          Master = FMODUnity.RuntimeManager.GetBus ("bus:/Master");
+     }
+
+     void Update () 
+     {
+          Master.setVolume (MasterVolume);
+     }
+
+     public void MasterVolumeLevel (float newMasterVolume)
+     {
+          MasterVolume = newMasterVolume;
+     }
+}
